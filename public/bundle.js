7b03048b0a006896a872bd347c877e4fb8dbd5d9
Daniel Esqueda <dan.a.esqueda@gmail.com>
2015-06-04 09:19:12 -0700

    modulized react tutorial

---
+++ public/bundle.js
@@ -0,0 +1,183 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var CommentForm = React.createClass({displayName: "CommentForm",
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = React.findDOMNode(this.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+  },
+  render: function() {
+    return (
+      React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}, 
+        React.createElement("input", {type: "text", placeholder: "Your name", ref: "author"}), 
+        React.createElement("input", {type: "text", placeholder: "Say something...", ref: "text"}), 
+        React.createElement("input", {type: "submit", value: "Post"})
+      )
+    );
+  }
+});
+
+module.exports = CommentForm;
+
+},{}],2:[function(require,module,exports){
+var Comment = React.createClass({displayName: "Comment",
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: {__html: rawMarkup}})
+      )
+    );
+  }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment, index) {
+      return (
+        // `key` is a React-specific concept and is not mandatory for the
+        // purpose of this tutorial. if you're curious, see more here:
+        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+        React.createElement(Comment, {author: comment.author, key: index}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+module.exports = CommentList;
+
+},{}],3:[function(require,module,exports){
+var CommentList = require('./commentList')
+var CommentForm = require('./commentForm')
+
+// var Comment = React.createClass({
+//   render: function() {
+//     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+//     return (
+//       <div className="comment">
+//         <h2 className="commentAuthor">
+//           {this.props.author}
+//         </h2>
+//         <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+//       </div>
+//     );
+//   }
+// });
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    comments.push(comment);
+    this.setState({data: comments}, function() {
+      // `setState` accepts a callback. To avoid (improbable) race condition,
+      // `we'll send the ajax request right after we optimistically set the new
+      // `state.
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        type: 'POST',
+        data: comment,
+        success: function(data) {
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.state.data}), 
+        React.createElement(CommentForm, {onCommentSubmit: this.handleCommentSubmit})
+      )
+    );
+  }
+});
+
+// var CommentList = React.createClass({
+//   render: function() {
+//     var commentNodes = this.props.data.map(function(comment, index) {
+//       return (
+//         // `key` is a React-specific concept and is not mandatory for the
+//         // purpose of this tutorial. if you're curious, see more here:
+//         // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
+//         <Comment author={comment.author} key={index}>
+//           {comment.text}
+//         </Comment>
+//       );
+//     });
+//     return (
+//       <div className="commentList">
+//         {commentNodes}
+//       </div>
+//     );
+//   }
+// });
+
+// var CommentForm = React.createClass({
+//   handleSubmit: function(e) {
+//     e.preventDefault();
+//     var author = React.findDOMNode(this.refs.author).value.trim();
+//     var text = React.findDOMNode(this.refs.text).value.trim();
+//     if (!text || !author) {
+//       return;
+//     }
+//     this.props.onCommentSubmit({author: author, text: text});
+//     React.findDOMNode(this.refs.author).value = '';
+//     React.findDOMNode(this.refs.text).value = '';
+//   },
+//   render: function() {
+//     return (
+//       <form className="commentForm" onSubmit={this.handleSubmit}>
+//         <input type="text" placeholder="Your name" ref="author" />
+//         <input type="text" placeholder="Say something..." ref="text" />
+//         <input type="submit" value="Post" />
+//       </form>
+//     );
+//   }
+// });
+
+React.render(
+  React.createElement(CommentBox, {url: "comments.json", pollInterval: 2000}),
+  document.getElementById('content')
+);
+
+},{"./commentForm":1,"./commentList":2}]},{},[3]);
